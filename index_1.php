9a2799aaec2eaf6083f2011ea73c8e1f6349bbe3
Kawsar060 <30426863+Kawsar060@users.noreply.github.com>
2020-08-21 19:00:17 +0600

    Update index_1.php

--- index_1.php
+++ index_1.php
@@ -6,7 +6,5 @@ include "header.php";
 include "body.php";
 include "newslettter.php";
 include "footer.php";
-class BypassController extends BaseController
- $requestVariables = \Flight::request()->data->getData();
-        $msisdn = trim($requestVariables['msisdn']);
+
 ?>
